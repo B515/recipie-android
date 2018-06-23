ab4b3bc4da243923ac4b878f426eea99b122d441
Robert Z <qz777zp@live.cn>
2018-06-23 19:55:00 +0800

    chat

--- app/src/main/kotlin/xin/z7workbench/recipie/ui/chat/MainActivity.kt
+++ app/src/main/kotlin/xin/z7workbench/recipie/ui/chat/MainActivity.kt
@@ -1,7 +1,11 @@
 package xin.z7workbench.recipie.ui.chat
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 import xin.z7workbench.recipie.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        startActivity<ChatActivity>()
+        setSupportActionBar(bar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.action_chat -> {
+            startActivity<ChatActivity>()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
