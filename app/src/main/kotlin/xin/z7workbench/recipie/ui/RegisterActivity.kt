0f577ef8b0a526c63ffb57d4be8ae020efce7fc7
Robert Z <qz777zp@live.cn>
2018-07-01 15:01:21 +0800

    Register
    register

--- app/src/main/kotlin/xin/z7workbench/recipie/ui/RegisterActivity.kt
+++ app/src/main/kotlin/xin/z7workbench/recipie/ui/RegisterActivity.kt
@@ -2,19 +2,20 @@ package xin.z7workbench.recipie.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.startActivity
 import xin.z7workbench.recipie.R
 
-class LoginActivity: AppCompatActivity() {
+class RegisterActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        login.setOnClickListener {
+        setContentView(R.layout.activity_register)
+        register.setOnClickListener {
             startActivity<MainActivity>()
         }
-        register.setOnClickListener {
-            startActivity<RegisterActivity>()
+
+        back.setOnClickListener{
+            onBackPressed()
         }
     }
 
