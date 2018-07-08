3b72f82dcb19c371af7ed0a35345320fd499197a
Robert Z <qz777zp@live.cn>
2018-07-08 09:04:30 +0800

    Recipe 5
    Comment

--- app/src/main/kotlin/xin/z7workbench/recipie/ui/UserInfoFragment.kt
+++ app/src/main/kotlin/xin/z7workbench/recipie/ui/UserInfoFragment.kt
@@ -12,7 +12,13 @@ import xin.z7workbench.recipie.R
 class UserInfoFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_user_info, container, false)
-        Glide.with(this).load(R.drawable.login_bg).into(view.bg)
+        view.apply {
+            Glide.with(this).load(R.drawable.login_bg).into(bg)
+            back.visibility = View.GONE
+            unfollow.visibility = View.GONE
+            follow.visibility = View.GONE
+            chat.visibility = View.GONE
+        }
         return view
     }
 }
