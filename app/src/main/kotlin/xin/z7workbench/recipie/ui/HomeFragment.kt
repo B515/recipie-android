796784a0b74b4b5b8a7893e50b19fc3518611b37
Robert Z <qz777zp@live.cn>
2018-07-08 19:44:11 +0800

    Home
    scroll

--- app/src/main/kotlin/xin/z7workbench/recipie/ui/HomeFragment.kt
+++ app/src/main/kotlin/xin/z7workbench/recipie/ui/HomeFragment.kt
@@ -5,12 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.item_recipe_display.view.*
 import xin.z7workbench.recipie.R
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val peopleAdapter = PeopleAdapter()
+        val llm = object : LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+
+        view.apply {
+            people.apply {
+                adapter = peopleAdapter
+                layoutManager = llm
+            }
+
+        }
+
         return view
+    }
+
+    class PeopleAdapter(val list: List<String> = mutableListOf("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1")) : RecyclerView.Adapter<PeopleAdapter.PeopleViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleViewHolder =
+                PeopleViewHolder(LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_recipe_display, parent, false))
+
+        override fun getItemCount() = list.size
+
+        override fun onBindViewHolder(holder: PeopleViewHolder, position: Int) {
+            holder.v.apply {
+                description.text = list[position]
+            }
+        }
+
+        class PeopleViewHolder(val v: View) : RecyclerView.ViewHolder(v)
     }
 }
